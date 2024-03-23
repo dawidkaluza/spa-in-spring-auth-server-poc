89fb8df1e0a8d8d141a983c60617609ab0ffe7f2
Dawid Kałuża <dkaluza27@gmail.com>
2024-03-23 13:58:11 +0100

    create view for sign in page

--- web/src/SignIn.js
+++ web/src/SignIn.js
@@ -1,11 +1,61 @@
 import React from "react";
 
 const SignIn = () => {
+
+  const handleFieldChange = (event) => {
+    // TODO impl
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    //TODO impl
+  };
+
   return (
-    <>
-      Sign in page.
-    </>
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        marginTop: "5em",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>Sign in</h1>
+
+      <Input
+        label={"Username"}
+        name={"username"}
+        value={"admin"}
+        onChange={handleFieldChange}
+      />
+      <Input
+        label={"Password"}
+        name={"password"}
+        type={"password"}
+        value={"passwd"}
+        onChange={handleFieldChange}
+      />
+
+      <input type={"submit"} value={"Submit"} />
+    </form>
   )
 };
 
-export { SignIn };
+const Input = ({ label, name, type = "text", value, onChange}) => {
+  return (
+    <div>
+      <span>{label}: </span>
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export {SignIn};
